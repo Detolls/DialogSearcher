4d86fd5fc91d750eb7f74cc93db2c0e01319eb90
Detolls <ZeroOneTur@yandex.ru>
2022-07-06 03:07:29 +0700

    Add API endpoints xml comments

--- DialogSearcher/DialogSearcher.WebAPI/Program.cs
+++ DialogSearcher/DialogSearcher.WebAPI/Program.cs
@@ -5,7 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 // DI Services
 builder.Services.AddTransient<IMessengerService, MessengerService>();

ba382fe70c3366a62a7140b285d713440874d6cc
Detolls <ZeroOneTur@yandex.ru>
2022-07-06 03:06:29 +0700

    Add Messenger controller

---
+++ DialogSearcher/DialogSearcher.WebAPI/Controllers/MessengerController.cs
@@ -0,0 +1,25 @@
+﻿using DialogSearcher.WebAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DialogSearcher.WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MessengerController : ControllerBase
+{
+    private readonly IMessengerService _messengerService;
+
+    public MessengerController(IMessengerService messengerService)
+    {
+        _messengerService = messengerService ?? throw new ArgumentNullException(nameof(messengerService));
+    }
+    
+    /// <summary>
+    /// Search clients dialog by client ids
+    /// </summary>
+    /// <param name="clientIds">Client ids</param>
+    /// <returns>Clients dialog id</returns>
+    [HttpPost("dialog/search")]
+    public IActionResult SearchDialog(Guid[] clientIds) 
+        => Ok(_messengerService.SearchDialogByClientIds(clientIds));
+}
